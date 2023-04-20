2428608c9ad8ee7056cec2ac80d46e58e64f4dda
Simone Tassotti <37712183+TassoMatto@users.noreply.github.com>
2023-04-20 09:33:48 +0200

    Aggiornamento file
    Aggiunta commenti

--- Interfaces/AuthenticationInterface.java
+++ Interfaces/AuthenticationInterface.java
@@ -3,15 +3,39 @@ package Interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * 
+ * @fun                             AuthenticationInterface
+ * @brief                           Interfaccia per la gestione dell'autenticazione di un client sul server
+ * @author                          Simone Tassotti
+ * @date                            20/04/2023
+ * 
+ */
 public interface AuthenticationInterface extends Remote {
     
-    public boolean register(String username, String password) throws RemoteException;
+    /**
+     * 
+     * @fun                                 register
+     * @brief                               Registrazione di un client sul server di gioco
+     * @param username                      Username dell'utente
+     * @param password                      Password dell'utente
+     * @return                              (1) Utente già registrato
+     *                                      (0) Utente registrato correttamente
+     *                                      (-1) Altrimenti
+     * @throws RemoteException
+     */
+    public int register(String username, String password) throws RemoteException;
 
-    public long login(String username, String password) throws RemoteException;
-
+    /**
+     * 
+     * @fun                                 registerClient
+     * @brief                               Registrazione del client al servizio di notifiche del server
+     * @param sn                            Riferimento RMI del client
+     * @param username                      Username dell'utente
+     * @param password                      Password dell'utente
+     * @throws RemoteException              
+     * @throws IllegalAccessException
+     */
     public void registerClient(ServerNotify sn, String username, String password) throws RemoteException, IllegalAccessException;
-
-    public void unregisterClient(String username, String password) throws RemoteException, IllegalAccessException;
-
 
 }
--- Interfaces/ServerNotify.java
+++ Interfaces/ServerNotify.java
@@ -4,10 +4,24 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.LinkedList;
 
+/**
+ * 
+ * @interface                   ServerNotify
+ * @brief                       Interfaccia per gestire le notifiche da parte del server
+ * @author                      Simone Tassotti
+ * @date                        20/04/2023
+ * 
+ */
 public interface ServerNotify extends Remote {
     
+    /**
+     * 
+     * @fun                                 ServerAlert
+     * @brief                               Manda un alert al client
+     * @param update                        Messaggi inviati dal server
+     * @throws RemoteException
+     * 
+     */
     public void ServerAlert(LinkedList<String> update) throws RemoteException;
-
-    public void EndGame(String oldWord) throws RemoteException;
 
 }
